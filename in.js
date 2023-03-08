304f15d698df530bac48198fa8173f72c65c889f
absabur <125259077+absabur@users.noreply.github.com>
2023-03-08 17:49:04 +0600

    Update in.js

--- in.js
+++ in.js
@@ -1,13 +1,27 @@
-for(i=0 ; i<26 ; i++){
-    var btn = document.querySelectorAll(".myButton")[i].addEventListener("click",function(){
+const voice = document.getElementById("voice");
 
-        var text = this.innerHTML;
-        console.log(text);
-        audioMalePlay(text)
-        animation(text);
+alert("Select the voice first, otherwise the voice won't play.")
+
+const selectVoice = (event) =>{
+
+    for(i=0 ; i<26 ; i++){
+        var btn = document.querySelectorAll(".myButton")[i].addEventListener("click",function(){
+            
+             var text = this.innerHTML;
+            if(event.target.value == "Male"){
+                Male(text);
+            }
+            if(event.target.value == "Female"){
+                Female(text);
+            }
+            animation(text);
+        
+        })
+    }
     
-    })
 }
+voice.addEventListener("change",selectVoice);
+
 
 document.addEventListener("keypress",function(event){
     var text = event.key;
@@ -15,13 +29,29 @@ document.addEventListener("keypress",function(event){
     animation(text);
 })
 
-function audioMalePlay(text){
+
+function Male(text){
     
     var aray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     for(i=0 ; i < aray.length ; i++){
         switch (text) {
             case aray[i]:
                 var audio = new Audio("audio/male/"+aray[i]+".mp3");
+                audio.play();
+                break;
+            
+        }
+    }
+    
+}
+
+function Female(text){
+    
+    var aray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    for(i=0 ; i < aray.length ; i++){
+        switch (text) {
+            case aray[i]:
+                var audio = new Audio("audio/"+aray[i]+".mp3");
                 audio.play();
                 break;
             
